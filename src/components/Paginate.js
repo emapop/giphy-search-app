5f97995600b2ed436f282e287c5f661ac301f834
emapop <ematabita14@gmail.com>
2022-09-15 19:34:45 +0300

    refactored the scss code

--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Paginate = (props) => {
-    const pageNumbers = [];
-    for(let i = 1; i <= Math.ceil(props.totalItems/props.itemsPerPage); i++) {
-        pageNumbers.push(i);
-
-    }
-    return <nav className="nav-pagination">
-        <ul className="pagination pagination-sm justify-content-end border-0">
-            {pageNumbers.map((number, index) => {
-                return (
-                    <li key={index} className="page-item">
-                        <a onClick = {()=> props.pageSelected(number)}href="#" className="page-link">{number}</a>
-                    </li>
-                )
-            })}
-        </ul>
+  const pageNumbers = [];
+  //this function divides the total amount of gifs retrived with the number of item assigned in itemsPerPage state variable which is passed as a prop
+  for (let i = 1; i <= Math.ceil(props.totalItems / props.itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav className="nav-pagination">
+      <ul className="pagination">
+        {/* this function displays the page number dynamically from the pageNumbers array */}
+        {pageNumbers.map((number, index) => {
+          return (
+            <li key={index} className="page-item">
+              {/*If the page number is clicked you will access that page with gifs */}
+              <a
+                onClick={() => props.pageSelected(number)}
+                href="#"
+                className="page-link"
+              >
+                {number}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </nav>
-}
+  );
+};
 export default Paginate;
--- src/components/TheSearch.js
+++ src/components/TheSearch.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import Paginate from './Paginate';
-const GIPHY_API = "https://api.giphy.com/v1/gifs/search?api_key=VYmsxlWOIM2Vqxw7gIXcaw4NE1DS0Gxc&q=";
+import React, { useState, useEffect, useCallback } from "react";
+import Paginate from "./Paginate";
+const GIPHY_API =
+  "https://api.giphy.com/v1/gifs/search?api_key=VYmsxlWOIM2Vqxw7gIXcaw4NE1DS0Gxc&q=";
 
 let TheSearch = () => {
-  const [ darkMode, setDarkMode ] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
   const [search, setSearch] = useState("", () => {
     // getting stored value
     const saved = localStorage.getItem(currentSearch);
@@ -14,107 +15,131 @@ let TheSearch = () => {
   const [loadingState, setLoadingState] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
+  //indexOfLastItem sets the last index item
   const indexOfLastItem = currentPage * itemsPerPage;
+  //indexOfFirst item sets the index which must almost be 0 to the first item
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  //currentItems return an array copy with gifs that have the first index returned as the indexOfFirstItem and the last index as idexOfLastItem
   const currentItems = gifs.slice(indexOfFirstItem, indexOfLastItem);
   const [items, setItems] = useState([]);
-  const [currentSearch, setCurrent] = useState(GIPHY_API+search);
+  const [currentSearch, setCurrent] = useState(GIPHY_API + search);
   console.log(currentSearch);
-
-useEffect(() => {
-  localStorage.setItem(currentSearch, JSON.stringify(search));
-  const items = JSON.parse(localStorage.getItem('search'));
-  if (items) {
-    setItems(items);
-   }
-}, [search, items]);
- 
-  let searchGif = useCallback( () => {
-    if(search.length > 0){
+  // this useEffect callback sets the local storage by extracting the search value and the giphy api which
+  //works as a key for storing searches in local storage
+  useEffect(() => {
+    setCurrent(GIPHY_API + search);
+    localStorage.setItem(currentSearch, JSON.stringify(search));
+    const items = JSON.parse(localStorage.getItem("search"));
+    if (items) {
+      setItems(items);
+    }
+  }, [search, items, currentSearch]);
+  // the useCallback retrieves the api search and returns the result as gif.images
+  let searchGif = useCallback(() => {
+    if (search.length > 0) {
       setLoadingState(true);
       fetch(currentSearch)
-      .then((res)=>{
-        setCurrent(currentSearch)
-        setLoadingState(false);
-        return res.json();
-      })
-      .then((result)=>{
-        setGifs(result.data.map((gif)=>{
-          return gif.images.fixed_height.url;
-        }))
-      })
-      .catch(()=>{
-        alert("Something went wrong");
-        setLoadingState(false);
-      })
+        .then((res) => {
+          setCurrent(currentSearch);
+          setLoadingState(false);
+          return res.json();
+        })
+        .then((result) => {
+          console.log(result);
+          setGifs(
+            result.data.map((gif) => {
+              return gif.images.fixed_height.url;
+            })
+          );
+        })
+        .catch(() => {
+          alert("Something went wrong");
+          setLoadingState(false);
+        });
     }
-  }, [currentSearch, loadingState])
+  }, [currentSearch, loadingState, search.length]);
   const pageSelected = (pageNumber) => {
     setCurrentPage(pageNumber);
-  }
+  };
+  // this useEffect callback activates the dark mode
   useEffect(() => {
-    const toggle = document.querySelector('.toggle-inner');
-    const main = document.querySelector('.main');
-    const body = document.querySelector('body');
-    if( darkMode === true ) {
-      main.classList.add('dark-mode');
-      body.classList.add('dark-mode');
-      toggle.classList.add('toggle-active');
+    const toggle = document.querySelector(".toggle-inner");
+    const main = document.querySelector(".main");
+    const body = document.querySelector("body");
+    if (darkMode === true) {
+      main.classList.add("dark-mode");
+      body.classList.add("dark-mode");
+      toggle.classList.add("toggle-active");
     } else {
-      body.classList.remove('dark-mode')
-      main.classList.remove('dark-mode');
-      toggle.classList.remove('toggle-active');
+      body.classList.remove("dark-mode");
+      main.classList.remove("dark-mode");
+      toggle.classList.remove("toggle-active");
     }
   }, [darkMode]);
   return (
     <div className="main">
       <div
         id="toggle"
-        onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)}
+        //when the toggle button is pressed the dark mode is activated
+        onClick={() =>
+          darkMode === false ? setDarkMode(true) : setDarkMode(false)
+        }
       >
-        <div className="toggle-inner"/>
+        <div className="toggle-inner" />
       </div>
       <div className="header">
         <div>
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="Search GIF"
+            //the value for the search input is extracted and stored in te search variable
             value={search}
-            onChange={(e)=>setSearch(e.target.value)} 
+            onChange={(e) => setSearch(e.target.value)}
           />
-          <button onClick={searchGif}>
+          <button
+            //when the button is clicked searchGif is called, this function extracts the value from the search variable
+            onClick={searchGif}
+          >
             Search
           </button>
         </div>
       </div>
-      <Paginate pageSelected = {pageSelected} currentPage = {currentPage} itemsPerPage={itemsPerPage} totalItems={gifs.length}/>
+      <Paginate
+        //this is the pagination section which deals with the amount of gifs per page and divides the pages according to the value set in itemsPerPage
+        pageSelected={pageSelected}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        totalItems={gifs.length}
+      />
       <div className="result">
-        {(!search) ? (<div className="message message-for-searching">
-            <p>Click the search bar and type some gifs names you want to see...🌟</p>
-        </div>) : (<></>)}
-        {
-          (loadingState) ? (
-            <div className="loading">
-              <div className="loader">
-              </div>
-            </div>
-          ) : (
-            <div className="list">
-              {
-                currentItems.map((gif)=>{
-                  return (
-                    <div key={gif.id} className="item">
-                      <img src={gif} alt="gif"/>  
-                    </div>
-                  )
-                })
-              }
-            </div>
-          )
-        }
+        {!search ? (
+          <div className="message message-for-searching">
+            <p>
+              Click the search bar and type some gifs names you want to see...🌟
+            </p>
+          </div>
+        ) : (
+          <></>
+        )}
+        {loadingState ? (
+          <div className="loading">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div className="list">
+            {/*this is the function that retrives the search result */}
+            {currentItems.map((gif) => {
+              return (
+                <div key={gif.id} className="item">
+                  <img src={gif} alt="gif" />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TheSearch;
